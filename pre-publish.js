e8e3e59b86d886da7b8cf61b90b48b2a91b933eb
Ankur Dutta <ankur.611@gmail.com>
2022-04-02 01:39:09 +0530

    pre-publish script

---
+++ pre-publish.js
@@ -0,0 +1,30 @@
+import { readFile, writeFile, copyFile } from 'fs/promises';
+
+(async () => {
+  const packageJson = await readFile('./package.json', { flag: 'r+', encoding: 'utf-8' });
+  const packageObj = JSON.parse(packageJson);
+
+  const libraryJson = await readFile('./library.json', { flag: 'r+', encoding: 'utf-8' });
+  const libraryObj = JSON.parse(libraryJson);
+
+  const libraryPackageObj = { ...packageObj, ...libraryObj };
+  const deleteList = [
+    'type',
+    'scripts',
+    'dependencies',
+    'devDependencies',
+    'volta',
+  ];
+  for (const property in libraryPackageObj) {
+    if (deleteList.includes(property)) {
+      delete libraryPackageObj[property];
+    }
+  }
+
+  await writeFile(
+    './dist/simple-timeout/package.json',
+    JSON.stringify(libraryPackageObj, undefined, 2),
+    { flag: 'w+' }
+  );
+  await copyFile('./LICENSE', './dist/simple-timeout/LICENSE');
+})()
